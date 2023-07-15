7160bf1ae0f27e0b8b3175e42d8f6fc877de3040
LexYu <84486286+TLexYuW@users.noreply.github.com>
2023-07-15 13:58:46 +0800

    update and fix

--- Dockerfile/ubuntu-gstreamer-build-from-source/cmd.sh
+++ Dockerfile/ubuntu-gstreamer-build-from-source/cmd.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
 echo "cd to /home"
-cd /home 
+cd /home
 
 echo "use pip3 install higher version meson"
 apt-get update && pip3 install meson
 
-echo "git clone gstreamer"
-git clone https://gitlab.freedesktop.org/gstreamer/gstreamer.git
+# echo "git clone gstreamer"
+# git clone https://gitlab.freedesktop.org/gstreamer/gstreamer.git
 
-echo "source, apt-key, tee, install kurento-media-server process"
-source /etc/upstream-release/lsb-release 2>/dev/null || source /etc/lsb-release
+if [ -x "/usr/bin/kurento-media-server" ]
+then 
+    echo "media server already installed"
 
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 234821A61B67740F89BFD669FC8A16625AFA7A83 
+else
+    echo "source, apt-key, tee, install kurento-media-server process"
+    source /etc/upstream-release/lsb-release 2>/dev/null || source /etc/lsb-release
 
-tee "/etc/apt/sources.list.d/kurento.list" >/dev/null <<EOF
-deb [arch=amd64] http://ubuntu.openvidu.io/7.0.0 $DISTRIB_CODENAME main
+    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 234821A61B67740F89BFD669FC8A16625AFA7A83
+
+    tee "/etc/apt/sources.list.d/kurento.list" >/dev/null <<EOF
+    deb [arch=amd64] http://ubuntu.openvidu.io/7.0.0 $DISTRIB_CODENAME main
 EOF
 
-apt-get update && apt-get install -y kurento-media-server
-echo "midea server install done!"
+    apt-get update && apt-get install -y kurento-media-server
+    echo "midea server install done!"
+
+fi
 
 echo "replace default media server configs"
 cp -r /home/configs/. /etc/kurento/modules/kurento
